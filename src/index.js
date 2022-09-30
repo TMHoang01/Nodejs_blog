35637e78994b37993ab1c1417a60f80ceb5d3388
TMHoang01 <tmhoang@DESKTOP-9HV2DN4.localdomain>
2022-09-30 15:57:19 +0700

    static file ang scss

--- src/index.js
+++ src/index.js
@@ -5,24 +5,26 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3005;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 // HTTP logger
 app.use(morgan('combined'));
 
 // Template engine
 app.engine('hbs', handlebars.engine({
-    defaultLayout: 'main',
-    extname: '.hbs'
+  defaultLayout: 'main',
+  extname: '.hbs'
 }));
 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources', 'views'));
 
 app.get('/', (req, res) => {
-    res.render('home');
+  res.render('home');
 })
 
 app.get('/news', (req, res) => {
-    res.render('news');
+  res.render('news');
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
