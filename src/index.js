61bb8e7b98ae273d3bdbe51377645394841dfa58
TMHoang01 <tmhoang@DESKTOP-9HV2DN4.localdomain>
2022-09-30 23:43:07 +0700

    Refactor code: create router , controller

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ const express = require('express');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
 const app = express();
-const port = 3005;
+const port = 3000;
 
+const route = require('./routes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -24,20 +25,10 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources', 'views'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
 
-app.get('/news', (req, res) => {
-  res.render('news');
-})
+route(app);
 
-app.get('/search', (req, res) => {
-  res.render('search');
-})
 
-app.post('/search', (req, res) => {
-  res.send('search'+ JSON.stringify( req.body));
-})
+
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
---
+++ src/routes/site.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const siteController = require('../app/controllers/SiteController.js');
+
+router.get('/:slug', siteController.search);
+router.get('/search', (req, res) => {
+    res.render('search');
+})
+
+router.post('/search', (req, res) => {
+    res.send('search'+ JSON.stringify( req.body));
+})
+
+router.get('/', siteController.index);
+
+module.exports = router;
