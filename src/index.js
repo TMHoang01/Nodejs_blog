93b32c7d9bb96c5b7b6c1c5dab3902651c20caae
TMHoang01 <tmhoang@DESKTOP-9HV2DN4.localdomain>
2022-09-30 17:32:59 +0700

    post and get

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3005;
 
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 // HTTP logger
@@ -25,6 +30,14 @@ app.get('/', (req, res) => {
 
 app.get('/news', (req, res) => {
   res.render('news');
+})
+
+app.get('/search', (req, res) => {
+  res.render('search');
+})
+
+app.post('/search', (req, res) => {
+  res.send('search'+ JSON.stringify( req.body));
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
